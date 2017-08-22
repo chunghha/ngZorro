15a621bd1b42ac658075328130329fbbc3aeba0c
Chris Ha <chunghha@users.noreply.github.com>
2017-08-22 01:27:50 -0500

    chore() Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, VERSION} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,8 @@ import {Component, OnInit, VERSION} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  validateForm: FormGroup;
+
   isCollapsed = false;
   isVisible = false;
   current = 1;
@@ -34,6 +37,12 @@ export class AppComponent implements OnInit {
       address: 'Sidney No. 1 Lake Park',
     }
   ];
+
+  _submitForm() {
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[ i ].markAsDirty();
+    }
+  }
 
   _displayDataChange($event) {
     this._displayData = $event;
@@ -77,7 +86,42 @@ export class AppComponent implements OnInit {
         this.isVisible = false;
       }
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {}
+  updateConfirmValidator() {
+    /** wait for refresh value */
+    setTimeout(_ => {
+      this.validateForm.controls[ 'checkPassword' ].updateValueAndValidity();
+    });
+  }
+
+  confirmationValidator = (control: FormControl): { [s: string]: boolean } => {
+    if (!control.value) {
+      return { required: true };
+    } else if (control.value !== this.validateForm.controls[ 'password' ].value) {
+      return { confirm: true, error: true };
+    }
+  }
+
+  getCaptcha(e: MouseEvent) {
+    e.preventDefault();
+  }
+
+  ngOnInit() {
+    this.validateForm = this.fb.group({
+      email            : [ null, [ Validators.email ] ],
+      password         : [ null, [ Validators.required ] ],
+      checkPassword    : [ null, [ Validators.required, this.confirmationValidator ] ],
+      nickname         : [ null, [ Validators.required ] ],
+      phoneNumberPrefix: [ '+86' ],
+      phoneNumber      : [ null, [ Validators.required ] ],
+      website          : [ null, [ Validators.required ] ],
+      captcha          : [ null, [ Validators.required ] ],
+      agree            : [ false ]
+    });
+  }
+
+  getFormControl(name) {
+    return this.validateForm.controls[ name ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 
@@ -17,6 +17,7 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     NgZorroAntdModule.forRoot()
   ],
   providers: [],
