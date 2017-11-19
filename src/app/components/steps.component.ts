af2c1ea3a67690e50b934a9891cbb9d0633a2ad4
Chris Ha <chunghha@users.noreply.github.com>
2017-11-19 17:08:58 -0600

    feat: steps with buttons

--- src/app/components/steps.component.ts
+++ src/app/components/steps.component.ts
@@ -1,7 +1,54 @@
 import { Component } from '@angular/core';
 
+import { NzMessageService } from 'ng-zorro-antd';
+
 @Component({
   selector: 'app-steps',
+  styleUrls: ['steps.component.scss'],
   templateUrl: './steps.component.html'
 })
-export class StepsComponent {}
+export class StepsComponent {
+  current = 0;
+  index = 'Login-content';
+
+  pre() {
+    this.current -= 1;
+    this.changeContent();
+  }
+
+  next() {
+    this.current += 1;
+    this.changeContent();
+  }
+
+  done() {
+    this._message.success('done');
+  }
+
+  changeContent() {
+    switch (this.current) {
+      case 0: {
+        this.index = 'Login-content';
+        break;
+      }
+      case 1: {
+        this.index = 'Verification-content';
+        break;
+      }
+      case 2: {
+        this.index = 'Pay-content';
+        break;
+      }
+      case 3: {
+        this.index = 'Done-content';
+        break;
+      }
+      default: {
+        this.index = 'error';
+      }
+    }
+  }
+
+  constructor(private _message: NzMessageService) {}
+
+}
